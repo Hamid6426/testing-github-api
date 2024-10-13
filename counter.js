862b075c83952fc0580faad007ead91325832f83
Mian Hamid Ur Rehman <mianhamid6426@gmail.com>
2024-10-13 16:05:12 +0500

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,9 @@
+function (counter) {
+  return coounter + 1;
+   console.log(`Count: ${counter}`);
+}
+
+// function (counter) {
+//  return coounter + 1;
+//  console.log(`Count: ${counter}`);
+// }
